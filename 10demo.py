8f27474f58a0b6114c79bff8307fdc2877058cf6
Madeline <mcccarson@ucdavis.edu>
2026-01-25 22:24:48 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -25,3 +25,34 @@ def pythagoras(a,b):
 	return c
 
 print(pythagoras(3,4))
+
+def circle_area(r): return math.pi * r**2
+def rectangle_area(w,h): return w * h
+
+print(circle_area(5))
+print(rectangle_area(4,10))
+
+s = 'hello world'
+print(s, type(s))
+
+a = 2
+b = 2
+if a == b:
+	print('a equals b')
+
+def is_even(x):
+	if x % 2 == 0: return True
+	return False
+
+print(is_even(2))
+print(is_even(3))
+
+if a < b:
+	print('a < b')
+elif a > b:
+	print('a > b')
+else:
+	print('a == b')
+
+if math.isclose(a, b): print('close enough')
+
