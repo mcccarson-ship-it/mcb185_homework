29453b5eb382b79b913416f841c9d1067db97cbd
Madeline <mcccarson@ucdavis.edu>
2026-01-25 21:36:12 -0800

    update

--- 10demo.py
+++ 10demo.py
@@ -7,3 +7,21 @@ print(1.5e-2)
 print(2 ** 6)
 
 print(5 % 2)
+
+print(0.1 * 1)
+
+print(0.1 * 3)
+
+import math
+
+a = 3                         # side of triangle
+b = 4                         # side of triangle
+c = math.sqrt(a**2 + b**2)    # hypotenuse
+
+print(type(a), type(b), type(c), sep=', ', end='!\n')
+
+def pythagoras(a,b):
+	c = math.sqrt(a**2 + b**2)
+	return c
+
+print(pythagoras(3,4))
